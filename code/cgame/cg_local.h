7d6ddf262ef5867aaf6ba34c2597bf96845d5dcc
Ernie Miller <ernie@ernie.io>
2026-05-24 19:34:09 -0400

    Add verified/admin indicator to scoreboard
    Port from trinity cd85cf4. Adds green checkmark (verified) and gold
    star (admin) to the scoreboard icon slot. Moves VOIP overlay to the
    right of the Status column. Fixes spectator VOIP bug and feeder icon
    centering.

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -339,6 +339,7 @@ typedef struct {
 	int				botSkill;		// 0 = not bot, 1-5 = bot
 	qboolean		vrPlayer;
 	qboolean		voipEnabled;	// qtrue if client has VOIP support
+	int				trinityUserType;	// 0 = unauth, 1 = verified, 2 = admin
 
 	vec3_t			color1;
 	vec3_t			color2;
@@ -880,6 +881,8 @@ typedef struct {
 
 	qhandle_t	botSkillShaders[5];
 	qhandle_t	vrPlayerShader;
+	qhandle_t	verifiedShader;
+	qhandle_t	adminShader;
 
 	// wall mark shaders
 	qhandle_t	wakeMarkShader;
